02831af09b5c59f3811c8cbc0f9ea2981ea9cc53
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-08-25 13:30:38 +0700

    feat: export newsletter service

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 export { createGroupService } from './group.js';
 export { calculate } from './calculator.js';
 export { createProviderManager } from './providers/manager.js';
+export { createNewsletterService } from './newsletter.js';
 export { toMp3, toImage, toVideo } from './media/index.js';
