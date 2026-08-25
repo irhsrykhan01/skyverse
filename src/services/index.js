d209d0f94a4ccad687ba2d8a0e532c585e28659e
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-08-25 08:27:59 +0700

    feat(services): wire provider and media services

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,4 @@
 export { createGroupService } from './group.js';
 export { calculate } from './calculator.js';
+export { createProviderManager } from './providers/manager.js';
+export { toMp3, toImage, toVideo } from './media/index.js';
