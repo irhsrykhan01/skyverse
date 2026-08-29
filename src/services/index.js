20bbf2875f45330b9fac027c46b98154db005188
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-08-29 22:30:26 +0700

    feat(services): export capability engine

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 export { createGroupService } from './group.js';
+export { createCapabilityEngine } from './capabilities.js';
 export { calculate } from './calculator.js';
 export { createProviderManager } from './providers/manager.js';
 export { createNewsletterService } from './newsletter.js';
