5afac7c25f6532326c967f6263b6e323991c53bf
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-09-05 16:05:19 +0700

    fix Tic-Tac-Toe diagonal winning combination

--- src/games/tictactoe.js
+++ src/games/tictactoe.js
@@ -1,4 +1,4 @@
-const WIN_LINES = Object.freeze([[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,6],[2,5,8]]);
+const WIN_LINES = Object.freeze([[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]);
 const games = new Map();
 const playerGames = new Map();
 
