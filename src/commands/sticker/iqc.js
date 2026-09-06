400aa16929c4a6addd7eb54a7ec29281a33b1ecb
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-09-06 20:45:58 +0700

    feat: switch iqc command to Nexray

--- src/commands/sticker/iqc.js
+++ src/commands/sticker/iqc.js
@@ -2,7 +2,7 @@ import { replyWithProviderMedia } from '../../services/providers/media-response.
 
 export const command = {
   name: 'iqc',
-  description: 'Membuat IQC dari teks menggunakan Depay.',
+  description: 'Membuat IQC dari teks menggunakan Nexray.',
   category: 'sticker',
   aliases: [],
   usage: 'iqc <teks>',
@@ -11,9 +11,7 @@ export const command = {
   cooldown: 3000,
   cost: 10,
   async execute(ctx) {
-    const response = await ctx.providers.depay.iqc(ctx.parsed.args.join(' '));
-    // IQC is an image generator; send the generated image directly instead
-    // of forcing it through sticker conversion, which can fail on large PNGs.
-    await replyWithProviderMedia(ctx, response, 'image');
+    const response = await ctx.providers.makers.iqc(ctx.parsed.args.join(' '));
+    await replyWithProviderMedia(ctx, response, 'image', null, { baseUrl: ctx.config.nexrayBaseUrl });
   },
 };
